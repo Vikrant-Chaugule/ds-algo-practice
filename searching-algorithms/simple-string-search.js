fbfdabc3ad9d5bf79e10fd39f74312ed78dd34e7
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-11-19 10:43:19 +0530

    learned string search algorithm with naive approach

---
+++ searching-algorithms/simple-string-search.js
@@ -0,0 +1,12 @@
+// create a function to search a substring in given string and return the count of substring
+
+const simpleStringSearch = (string, substring) => {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < substring.length; j++) {
+            if (string[i + j] !== substring[j]) break;
+            if (j === substring.length - 1) count++;
+        }
+    }
+    return count;
+}
