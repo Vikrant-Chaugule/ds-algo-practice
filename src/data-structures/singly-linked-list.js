ea48f3ab9e9907e0ab8694a95b5583c36803e2f0
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-12-02 14:19:36 +0530

    implemented pop method for singly linked list

--- src/data-structures/singly-linked-list.js
+++ src/data-structures/singly-linked-list.js
@@ -28,6 +28,28 @@ class SinglyLinkedList {
     return this;
   }
 
+  // remove from the end
+  pop() {
+    if (!this.head) return;
+    else {
+      let currentNode = this.head;
+      let secondLastNode = currentNode;
+      while (currentNode.next) {
+        secondLastNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      this.tail = secondLastNode;
+      this.tail.next = null;
+      this.length--;
+      if (this.length <= 0) {
+        this.head = null;
+        this.tail = null;
+      }
+      return currentNode;
+    }
+  }
+
   print() {
     let node = this.head;
     for (let i = 0; i < this.length; i++) {
@@ -43,3 +65,11 @@ list.push(10);
 list.push(15);
 list.push(20);
 list.push(25);
+
+list.print(); // 5->10->15->20->25
+
+// 5->10->15->20->25
+list.pop(); // 5->10->15->20
+list.pop(); // 5->10->15
+list.pop(); // 5->10
+list.print(); // 5->10
